adf544e3db4ad6188b6f444e8cd5e7e85b2b59f3
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-07-31 14:36:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+
+    region = "us-east-1"
+
+}
+
+
+terraform {
+
+ backend "s3" {
+
+    region = "us-east-1"
+    bucket = "tfstate-nts16-2024"
+    dynamodb_table = "nts16-dynamodbtable"   
+    key = "dasa_demo_key.tfstate"
+
+   }
+
+}
+
+
+variable "keyname" {
+
+ type = string
+
+}
+
+
+
+resource "tls_private_key" "rsa" {
+
+algorithm = "RSA"
+
+rsa_bits = 4096
+
+}
+
+
+
+resource "aws_key_pair" "tf-key-pair" {
+
+key_name = var.keyname
+
+public_key = tls_private_key.rsa.public_key_openssh
+
+}
+
+
+
+resource "local_file" "tf-key" {
+
+content = tls_private_key.rsa.private_key_pem
+
+filename = var.keyname
+
+}
+
+
+resource "aws_instance" "web-server" {
+
+ ami      = "ami-0a0e5d9c7acc336f1"
+
+ instance_type = "t2.micro"
+
+ key_name   = var.keyname
+
+
+
+
+
+ 
+
+ provisioner "remote-exec" {
+
+   inline = [
+
+      "sudo apt-get update",
+      "sudo apt-get update",
+      "sudo apt install -y apache2",
+      "sudo chmod -R 777 /var/www/html/"
+
+	]
+
+  }
+
+	
+
+ provisioner "file" {
+
+  source   = "index.html"
+
+  destination = "/var/www/html/index.html"
+
+ }
+
+ connection {
+
+  user    = "ubuntu"
+
+  private_key = "${file(local_file.tf-key.filename)}"
+
+   host = "${aws_instance.web-server.public_ip}"
+
+ }
+
+}
